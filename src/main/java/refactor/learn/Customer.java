cf6f7ff43c2571670718889a162e990c237fc28c
Aziz Hararwala <ahararwala@pillartechnology.com>
2016-03-15 17:49:53 -0400

    ahararwala: added solution for chapter 1 for extracting and moving a method

--- src/main/java/refactor/learn/Customer.java
+++ src/main/java/refactor/learn/Customer.java
@@ -23,43 +23,22 @@ public class Customer {
 	public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
-		Iterator<Rental> rentals = _rentals.iterator();
 		String result = "Rental Record for " + getName() + "\n";
-		while (rentals.hasNext()) {
-			double thisAmount = 0;
-			Rental each = rentals.next();
-			// determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
-			}
 
-			// add frequent renter points
+		for (Rental rental : _rentals) {
 			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
+			if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+					&& rental.getDaysRented() > 1)
 				frequentRenterPoints++;
-			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t"
-					+ String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+			result += "\t" + rental.getMovie().getTitle() + "\t"
+					+ String.valueOf(rental.getCharge()) + "\n";
+			totalAmount += rental.getCharge();
 		}
-		// add footer lines
+
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
 		return result;
 	}
+
 }
