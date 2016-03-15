a1ca4689ab614942dd474e3ba5db94c09d0f4b61
Aziz Hararwala <ahararwala@pillartechnology.com>
2016-03-15 17:57:47 -0400

    ahararwala: added solution for chapter 1 for extracting and moving a frequent rental points

--- src/main/java/refactor/learn/Customer.java
+++ src/main/java/refactor/learn/Customer.java
@@ -3,42 +3,40 @@ package refactor.learn;
 import java.util.*;
 
 public class Customer {
-	private String _name;
-	private List<Rental> _rentals;
+    private String _name;
+    private List<Rental> _rentals;
 
-	public Customer(String name) {
-		_name = name;
-		_rentals = new ArrayList<Rental>();
+    public Customer(String name) {
+        _name = name;
+        _rentals = new ArrayList<Rental>();
 
-	}
+    }
 
-	public void addRental(Rental arg) {
-		_rentals.add(arg);
-	}
+    public void addRental(Rental arg) {
+        _rentals.add(arg);
+    }
 
-	public String getName() {
-		return _name;
-	}
+    public String getName() {
+        return _name;
+    }
 
-	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-		String result = "Rental Record for " + getName() + "\n";
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        String result = "Rental Record for " + getName() + "\n";
 
-		for (Rental rental : _rentals) {
-			frequentRenterPoints++;
-			if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& rental.getDaysRented() > 1)
-				frequentRenterPoints++;
-			result += "\t" + rental.getMovie().getTitle() + "\t"
-					+ String.valueOf(rental.getCharge()) + "\n";
-			totalAmount += rental.getCharge();
-		}
+        for (Rental rental : _rentals) {
+            frequentRenterPoints += rental.getFrequentRenterPoints();
 
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
-				+ " frequent renter points";
-		return result;
-	}
+            result += "\t" + rental.getMovie().getTitle() + "\t"
+                    + String.valueOf(rental.getCharge()) + "\n";
+            totalAmount += rental.getCharge();
+        }
+
+        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints)
+                + " frequent renter points";
+        return result;
+    }
 
 }
